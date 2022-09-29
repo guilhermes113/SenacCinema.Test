9825c9d24a4e43d7001ce6777a2be154203687fc
GUILHERME SANCHES DA ROCHA <guilherme.srocha11@senacsp.edu.br>
2022-09-29 20:16:54 -0300

    Coloca

---
+++ CinemaVendas.API/Controllers/FinancialController.cs
@@ -0,0 +1,29 @@
+﻿using CinemaVendas.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaVendas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FinancialController : ControllerBase
+    {
+        private readonly IFinancialsService _financialService;
+
+        public FinancialController(IFinancialsService financialsService)
+        {
+            _financialService = financialsService;
+        }
+
+        [HttpGet("Total")]
+        public IActionResult GetTotalSold()
+        {
+            return Ok(_financialService.GetTotalSold());
+        }
+
+        [HttpGet("Stats")]
+        public IActionResult GetStats()
+        {
+            return Ok(_financialService.GetStats());
+        }
+    }
+}
---
+++ CinemaVendas.API/Controllers/FoodController.cs
@@ -0,0 +1,28 @@
+﻿using CinemaVendas.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaVendas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        private readonly IFoodService _ticketService;
+
+        public FoodController(IFoodService ticketService)
+        {
+            _ticketService = ticketService;
+        }
+
+        [HttpGet]
+        public IActionResult GetFoodSold()
+        {
+            var result = _ticketService.GetAllSold();
+
+            if(result.Count < 1)
+                return NotFound(result);
+
+            return Ok(result);
+        }
+    }
+}
